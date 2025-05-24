25720c3b779150ca21512c3b8d355ea30b716ebf
Song Kun <bborn.pisces@gmail.com>
2025-05-24 10:50:57 +0800

    add ignore domain

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
     
-ignore_urls = ["1.1.1.1"  , "mtalk.google.com" , "android.apis.google.com" , "dns.google" , "8.8.8.8" , "gstatic" , "10.10." , "1.0.0.1" , "8.8.4.4" , "cloudflare"]
+ignore_urls = ["one.one.one.one", "1.1.1.1"  , "mtalk.google.com" , "android.apis.google.com" , "dns.google" , "8.8.8.8" , "gstatic" , "10.10." , "1.0.0.1" , "8.8.4.4" , "cloudflare"]
 
 top_N_target = 50
 
